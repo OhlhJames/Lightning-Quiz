3a8f31d1d5060c2656e1ee2a8350460c32ded804
IsItIzzet <ohlhjames@gmail.com>
2024-01-24 21:55:48 -0600

    Added front page function so that go Back button could be called from end of quiz more easily

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,14 +22,15 @@ var goBackBtn = document.createElement('button');
 initialsPrompt.appendChild(goBackBtn);
 goBackBtn.textContent = 'Go Back';
 initialsPrompt.style.display ='none'
-goBackBtn.addEventListener('click', function(){
-  startBtn.style.display = 'flex'
-  quizTimer = 5
-  header.textContent = "Lightning Quiz!";
-  mainEl.textContent = "Welcome to the Lightning Quiz! once you press the 'Start Quiz' button, you will have one minute to answer up to 6 questions as correctly and as fast as you can! I hope your ready for a challenge!"
-  initialsPrompt.style.display ='none'
-});
-
+goBackBtn.addEventListener('click', frontPage());
+  function frontPage(){
+    header.textContent = "Lightning Quiz!";
+    mainEl.textContent = "Welcome to the Lightning Quiz! once you press the 'Start Quiz' button, you will have one minute to answer up to 6 questions as correctly and as fast as you can! I hope your ready for a challenge! "
+    startBtn.style.display = 'block';
+    initialsPrompt.style.display = 'none';
+    highScoreTracker.style.display ='none';
+    answerList.style.display = 'none';
+  }
 function highScore(){
   quizTimer = 0;
   timeEl.textContent = '';
